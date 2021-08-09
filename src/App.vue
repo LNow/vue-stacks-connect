c3b0c722f6532ce3bddcc5ca977ee185d81245ef
Lukasz <829210+LNow@users.noreply.github.com>
2021-08-09 13:29:46 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <Auth />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Auth from "./components/Auth.vue";
+
+@Options({
+  components: {
+    Auth,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/Auth.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn flat @click="auth">Connect wallet</v-btn>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { AppConfig, showConnect, UserSession } from "@stacks/connect";
+
+export default class Auth extends Vue {
+  public auth() {
+    let appConfig = new AppConfig(["store_write", "publish_data"]);
+    let userSession = new UserSession({ appConfig });
+    console.info(userSession,appConfig)
+
+    showConnect({
+      appDetails: {
+        name: 'MyApp',
+        icon: '',
+      },
+      onCancel: () => {
+        console.error("CANCEL!!!")
+      }
+    });
+  }
+}
+</script>
